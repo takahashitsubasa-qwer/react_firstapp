ba1b15256c94daa6a0d73557b124289d772b2352
takahashitsubasa-qwer <qwer.nidalee@gmail.com>
2026-08-11 16:34:37 +0900

    update:tailwind_css_environment_construction

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-indigo-600">
+      <h1 className="text-5xl font-black text-white shadow-lg p-6 bg-indigo-600 rounded-2xl">
+        Tailwind CSS 反映成功！ 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
